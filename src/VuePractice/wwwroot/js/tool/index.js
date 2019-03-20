795de6a44044ed9b488f5c1888b02b4aa2e45ad1
qizhenlu <q@qizl.cn>
2019-03-20 18:17:29 +0800

    -

--- src/VuePractice/wwwroot/js/tool/index.js
+++ src/VuePractice/wwwroot/js/tool/index.js
@@ -1,6 +1,7 @@
-﻿import hello from '~/components/hello.vue'
+﻿import Vue from 'vue'
+import hello from '~/components/HelloWorld.vue'
 
-var app=new Vue({
+var app = new Vue({
     el: '#app',
-    render:c=>c(hello)
+    render: c => c(hello)
 })
