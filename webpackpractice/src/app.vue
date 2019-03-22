29d551a19193b543130634ef2ae8c32e086d78d1
qizhenlu <q@qizl.cn>
2019-03-22 18:21:03 +0800

    -

---
+++ webpackpractice/src/app.vue
@@ -0,0 +1,18 @@
+﻿<template>
+    <div>Hello {{name}}</div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                name:'Vue.js'
+            }
+        }
+    }
+</script>
+<style scoped>
+    div{
+        color:#f60;
+        font-size:24px;
+    }
+</style>
